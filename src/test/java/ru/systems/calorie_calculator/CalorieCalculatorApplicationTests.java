5cb497f3159be5cd94e0bcb79f56c003cac2c3c9
Arina Zubova <arina777pototskaya@gmail.com>
2025-03-18 22:54:52 +0400

    Init

---
+++ src/test/java/ru/systems/calorie_calculator/CalorieCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.systems.calorie_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
